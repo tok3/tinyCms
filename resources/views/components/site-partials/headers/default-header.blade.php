be48ac043f22d9cd2051c0dde095f743903e58f6
tok3 <tobias@eq3w.de>
2024-08-29 13:47:55 +0200

    temp logo

--- resources/views/components/site-partials/headers/default-header.blade.php
+++ resources/views/components/site-partials/headers/default-header.blade.php
@@ -54,8 +54,8 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-body">
             <div class="container py-lg-2 position-relative">
                 <a class="navbar-brand" href="{{ URL::asset('assan/index.html') }}">
-                    <img src="{{ URL::asset('assets/img/logo/logo.svg') }}" alt="" class="navbar-brand-light img-fluid">
-                    <img src="{{ URL::asset('assets/img/logo/logo-white.svg') }}" alt="" class="navbar-brand-dark img-fluid">
+                    <img src="{{ URL::asset('assets/img/logo/logo.png') }}" alt="" class="navbar-brand-light img-fluid">
+                    <img src="{{ URL::asset('assets/img/logo/logo-white.png') }}" alt="" class="navbar-brand-dark img-fluid">
                 </a>
 
                 <div class="d-flex align-items-center navbar-no-collapse-items order-lg-last">
