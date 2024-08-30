56a5cb73c67443adb5331fad74d038fb8144b902
tok3 <tobias@eq3w.de>
2024-08-30 16:48:24 +0200

    isset auf container id

--- resources/views/content-sections/text.blade.php
+++ resources/views/content-sections/text.blade.php
@@ -1,4 +1,4 @@
-<div @if($data['container_id'] != "")id="{{$data['container_id']}}"@endif @if($prevElem == "")style="margin-top:120px !important;"@endif class="container {{$data['margin_top']}} ">
+<div @if(isset($data['container_id']))id="{{$data['container_id']}}"@endif @if($prevElem == "")style="margin-top:120px !important;"@endif class="container {{$data['margin_top']}} ">
     <div class="row" style="@if($data['margin_bottom'] != "")margin-bottom:{{$data['margin_bottom']}}!important;@endif @if($data['margin_top'] != "")margin-top:{{$data['margin_top']}} !important;@endif">
         <div class="col-xl-9 mx-auto">
 
