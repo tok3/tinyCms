e21c4379587a29c0604c066c29fce6f03a40416d
tok3 <tobias@eq3w.de>
2024-08-30 16:51:48 +0200

    wathn

--- resources/views/content-sections/text.blade.php
+++ resources/views/content-sections/text.blade.php
@@ -1,14 +1,12 @@
-<div @if(isset($data['container_id']))id="{{$data['container_id']}}"@endif @if($prevElem == "")style="margin-top:120px !important;"@endif class="container {{$data['margin_top']}} ">
-    <div class="row" style="@if($data['margin_bottom'] != "")margin-bottom:{{$data['margin_bottom']}}!important;@endif @if($data['margin_top'] != "")margin-top:{{$data['margin_top']}} !important;@endif">
+<div @if(isset($data['container_id']))id="{{$data['container_id']}}" @endif @if($prevElem == "")style="margin-top:120px !important;" @endif class="container {{$data['margin_top']}} ">
+    <div class="row"
+         style="@if(isset($data['margin_bottom']) && $data['margin_bottom'] != 0)margin-bottom:{{$data['margin_bottom']}}!important;@endif @if(isset($data['margin_top']) && $data['margin_top'] != "")margin-top:{{$data['margin_top']}} !important;@endif">
         <div class="col-xl-9 mx-auto">
-
             @php
                 $levelHeadingTag = $data['levelHeading'] ?? 'h4'; // Standardwert ist h4, falls nicht gesetzt
                 $levelHeading2Tag = $data['levelHeading2'] ?? 'h4'; // Standardwert ist h4, falls nicht gesetzt
             @endphp
-
-            <{{ $levelHeadingTag }} class
-            ="mb-4">{!! $data['heading'] !!}</{{ $levelHeadingTag }}>
+            <{{ $levelHeadingTag }} class="mb-4">{!! $data['heading'] !!}</{{ $levelHeadingTag }}>
 
         @if(!empty($data['heading2']))
             <{{ $levelHeading2Tag }} class="mb-4">{!! $data['heading2'] !!}</{{ $levelHeading2Tag }}>
