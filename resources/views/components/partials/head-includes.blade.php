9db25c57439f38fd43040fd22161c0b94ec5dfb2
tok3 <tobias@eq3w.de>
2024-09-06 12:34:54 +0200

    update counter und co

--- resources/views/components/partials/head-includes.blade.php
+++ resources/views/components/partials/head-includes.blade.php
@@ -14,3 +14,6 @@
 <link rel="preconnect" href="{{ URL::asset('https://fonts.gstatic.com') }}" crossorigin>
 <link href="{{ URL::asset('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&family=Source+Serif+4:ital,wght@0,400;0,500;1,400;1,500&display=swap') }}" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+
+
