e1208b59d783fc797e286886792d3fdd396b8677
tok3 <tobias@eq3w.de>
2024-09-09 16:42:26 +0200

    update counter und co

--- resources/views/content-sections/countdown-timer.blade.php
+++ resources/views/content-sections/countdown-timer.blade.php
@@ -1,45 +1,15 @@
-
-<section class="{!! $data['background'] !!} text-white position-relative"><svg class="position-absolute top-0 start-0 text-white w-50 h-auto w-lg-75" width="136" height="76" viewBox="0 0 136 76" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-opacity=".1" d="M136 -28C136 -14.3425 133.31 -0.818782 128.083 11.7991C122.857 24.4169 115.196 35.8818 105.539 45.5391C95.8818 55.1964 84.4169 62.857 71.7991 68.0835C59.1812 73.31 45.6575 76 32 76C18.3425 76 4.81878 73.31 -7.79908 68.0835C-20.4169 62.857 -31.8818 55.1964 -41.5391 45.5391C-51.1964 35.8818 -58.857 24.4169 -64.0835 11.7991C-69.31 -0.818789 -72 -14.3425 -72 -28L136 -28Z" fill="currentColor"></path></svg>
-    <div class="container py-4  position-relative z-1">
-        <div class="row align-items-center justify-content-center">
-            <div class="col-md-10 col-lg-8 col-xl-7 text-center mx-auto">
-                <h2 class="mb-4 display-4 aos-init aos-animate" data-aos="fade-up">{!! $data['heading'] !!}</h2>
-                <p class="mb-5 px-lg-5 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">{!! $data['subtext'] !!}</p>
-
-                <div class="counter-container" style="whitespace:nowrap" data-countdown-end="{!! $data['endDate'] !!}">
-
-                    <div class="wrapper">
-                        <div class="item">
-                            <div class="number">
-                                <span id="days">00</span>
-                            </div>
-                            <span>Tage</span>
-                        </div>
-                        <div class="item">
-                            <div class="number">
-                                <span id="hours">00</span>
-                            </div>
-                            <span>Stunden</span>
-                        </div>
-                        <div class="item">
-                            <div class="number">
-                                <span id="minutes">00</span>
-                            </div>
-                            <span>Minuten</span>
-                        </div>
-                        <div class="item">
-                            <div class="number">
-                                <span id="seconds">00</span>
-                            </div>
-                            <span>Sekunden</span>
-                        </div>
-                    </div>
+<section class="position-relative">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-xl-9 mx-auto mb-5 mb-md-0">
+                <div>
+                    <h2 class="mb-4 w-lg-75 aos-init aos-animate" data-aos="fade-up"><span style="font-size: 31.84px;"><br />Unsere Expertise </span></h2>
+                    <p class="mb-7 mb-lg-9 lead aos-init aos-animate" data-aos="fade-up">Ihre Website nahtlos an die Barrierefreiheitsanforderungen anzupassen. Wir arbeiten gezielt daran, Ihre Seite schrittweise barrierefrei zu gestalten, ohne dabei das Nutzererlebnis oder die Funktionalität zu beeinträchtigen. Der gesamte Prozess erfolgt mit Leichtigkeit und ohne zusätzliche Belastungen für Ihr Team.</p>
                 </div>
-                @if( $data['buttonText'] != "")
-                <div class="aos-init aos-animate" data-aos="fade-up">
-                    <a href="{!! $data['buttonTarget'] !!}" class="btn {!! $data['buttonAppearance'] !!} btn-lg">{!! $data['buttonText'] !!}</a></div>
-                   @endif
+                <h2 class="mb-4 aos-init aos-animate" data-aos="fade-up">Unser Ziel </h2>
+                <p class="mb-5 aos-init aos-animate" data-aos="fade-up">den Übergang für Sie so reibungslos wie möglich zu gestalten, damit Sie sich auf Ihr Kerngeschäft konzentrieren können, während wir sicherstellen, dass Ihre Website den gesetzlichen Anforderungen entspricht. Durch den Einsatz bewährter Tools und Techniken garantieren wir Ihnen eine schnelle und unkomplizierte Umstellung – von der Konformität mit den WCAG 2.1-Standards bis hin zur Optimierung für barrierefreie mobile Anwendungen.</p>
+                <p class="mb-6 lead aos-init aos-animate" data-aos="fade-up">Lassen Sie uns diesen Schritt gemeinsam gehen und Ihre digitale Präsenz für alle zugänglich machen – stressfrei, effizient und rechtzeitig vor dem Stichtag.</p>
             </div>
-            <!--End Col--></div>
-        <!-- / .row --></div>
-    <!-- / .container --></section>
+        </div>
+    </div>
+</section>
