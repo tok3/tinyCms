595a3f3c11ca57b4ac6abd50cf945d10a3491e1a
tok3 <tobias@eq3w.de>
2024-09-18 14:46:24 +0200

    back to top removed

--- resources/views/components/site-partials/social-media-buttons.blade.php
+++ resources/views/components/site-partials/social-media-buttons.blade.php
@@ -3,19 +3,19 @@
     <div class="d-flex flex-wrap align-items-center">
         <a href="https://www.linkedin.com/company/aktion-barrierefrei-de-ist-eine-initiative-der-camindu-gmbh/?viewAsMember=true"
            class="d-block width-3x height-3x my-2 me-3 hover-lift hover-shadow rounded-circle">
-            <img src="http://localhost:8004/assets/img/brands/Linkedin.svg" alt="Social media LinkedIn-Button" class="rounded-circle w-100 h-auto">
+            <img src="{{asset('assets/img/brands/Linkedin.svg')}}" alt="Social media LinkedIn-Button" class="rounded-circle w-100 h-auto">
         </a>
         <a href="#!" class="d-block width-3x height-3x my-2 me-3 hover-lift hover-shadow rounded-circle">
-            <img src="http://localhost:8004/assets/img/brands/Facebook.svg" alt="Social media Facbook-Button" class="rounded-circle w-100 h-auto">
+            <img src="{{asset('assets/img/brands/Facebook.svg')}}" alt="Social media Facbook-Button" class="rounded-circle w-100 h-auto">
         </a>
         <a href="#!" class="d-block width-3x height-3x my-2 me-3 hover-lift hover-shadow rounded-circle">
-            <img src="http://localhost:8004/assets/img/brands/X.svg" alt="Social media X-Button, ehemals Twitter" class="rounded-circle w-100 h-auto">
+            <img src="{{asset('assets/img/brands/X.svg')}}" alt="Social media X-Button, ehemals Twitter" class="rounded-circle w-100 h-auto">
         </a>
         <a href="#!" class="d-block width-3x height-3x my-2 me-3 hover-lift hover-shadow rounded-circle">
-            <img src="http://localhost:8004/assets/img/brands/Instagram.svg" alt="Social media Instagram-Button" class="rounded-circle w-100 h-auto">
+            <img src="{{asset('assets/img/brands/Instagram.svg')}}" alt="Social media Instagram-Button" class="rounded-circle w-100 h-auto">
         </a>
         <a href="#!" class="d-block width-3x height-3x my-2 me-3 hover-lift hover-shadow rounded-circle">
-            <img src="http://localhost:8004/assets/img/brands/xing.svg" alt="Social media Xing-Button" class="rounded-circle w-100 h-auto">
+            <img src="{{asset('assets/img/brands/xing.svg')}}" alt="Social media Xing-Button" class="rounded-circle w-100 h-auto">
         </a>
 
     </div>
