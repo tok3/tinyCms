66b662debf8aef2681b3b7060400875039387041
tok3 <tobias@eq3w.de>
2024-09-20 12:38:00 +0200

    sitemap widget

---
+++ app/Filament/Widgets/SitemapGeneratorWidget.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class SitemapGeneratorWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.sitemap-generator-widget';
+    // Optional: to set the widget height
+    protected static ?int $contentHeight = 100;
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,7 +50,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\AccountWidget::class,
 
                 Widgets\FilamentInfoWidget::class,
             ])
