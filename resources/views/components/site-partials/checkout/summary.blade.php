2231a22ecb0f64d33d59d803a752be56b4bab2cf
tok3 <tobias@eq3w.de>
2024-10-18 13:09:01 +0200

    mit subscriptions beta

--- resources/views/components/site-partials/checkout/summary.blade.php
+++ resources/views/components/site-partials/checkout/summary.blade.php
@@ -50,6 +50,7 @@
                 <td class="align-text-top">Preis:&nbsp</td>
                 <td><strong class="total-price">{{$selectedProduct->formattedPrice}} €</strong> <small id="payment-modality">{!!  $paymentModality[$selectedProduct->interval]!!}</small></td>
             </tr>
+            -->{{$selectedProduct->trial_period_days}}
             @if($selectedProduct->trial_period_days > 0)
                 <tr id="trial-period-row">
                     <td class="align-text-top text-right text-danger" style="text-align: right !important;"><i class="bi bi-info-circle"></i>&nbsp;</td>
