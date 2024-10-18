0a96c4f4331b53dc2847d946847fc334dc76bc21
tok3 <tobias@eq3w.de>
2024-10-18 13:47:39 +0200

    subscriptions test

--- public/assets/js/checkout.js
+++ public/assets/js/checkout.js
@@ -130,7 +130,6 @@ $(document).ready(function () {
                         $('#payment-modality').html(paymentModality[response.interval]);
 
 
-                        console.log('response->'+responce);
                         if (response.trial_period_days > 0) {
                             var trialEnddate = addDaysToDate(response.trial_period_days);
 
