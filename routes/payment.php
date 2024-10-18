298c1c402f9f5f21fb38ee2a79950ddbc96484f8
tok3 <tobias@eq3w.de>
2024-10-18 11:43:00 +0200

    mit subscriptions beta

--- routes/payment.php
+++ routes/payment.php
@@ -9,22 +9,41 @@ use App\Http\Controllers\WebhookController;
 use App\Http\Controllers\CreateSubscriptionController;
 
 use App\Http\Controllers\MollieTestController;
+use App\Http\Controllers\MolliePaymentController;
 
+//subscriptiopn test
+Route::get('/subscription', [CheckoutController::class, 'subscription'])->name('subscription');
+
+Route::get('/mollietest', [CheckoutController::class, 'mollietest'])->name('mollietest');
+Route::get('/subscriptiontest', [CheckoutController::class, 'subscriptiontest'])->name('subscriptiontest');
+
+Route::post('/check-email', [CheckoutController::class, 'checkEmail']);
+
+Route::get('/get-product-details', [CheckoutController::class, 'getProductDetails']);
+//
+Route::get('/mollie/payment/test', [MolliePaymentController::class, 'test']);
+
+Route::post('/mollie/payment', [MolliePaymentController::class, 'handlePaymentNotification'])->name('mollie.paymentWebhook');
+Route::post('/mollie/subscription', [MolliePaymentController::class, 'handleSubscriptionWebhook'])->name('mollie.subscriptionWebhook');
+
+Route::get('/get-product-details', [CheckoutController::class, 'getProductDetails'])->name('getProductDetails'); // route für ajax um beim checkout das produkt vom server zu holen um die zusammenfassung in smartwizzard anzuzeigen
+// ende testroute
 
 Route::get('/preise', [CheckoutController::class, 'showPlans'])->name('view.plans');
 
 Route::post('/checkout', [CheckoutController::class, 'preparePayment'])->name('checkout.plan');
 Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('order.success');
-Route::get('/webhooks/mollie', [CheckoutController::class, 'handleWebhookNotification'])->name('webhooks.mollie');
-Route::post('/webhooks/mollie/first-payment', [CheckoutController::class, 'handleWebhookNotification'])->name('webhooks.mollie-first');
-Route::get('/webhooks/mollie/first-payment', [CheckoutController::class, 'handleWebhookNotification'])->name('webhooks.mollie-frst');
+
+Route::post('/webhooks/mollie', [CheckoutController::class, 'handleWebhookNotification'])->name('ng-webhook');
+/*Route::post('/webhooks/mollie/first-payment', [CheckoutController::class, 'handleWebhookNotification'])->name('webhooks.mollie-first');
+Route::get('/webhooks/mollie/first-payment', [CheckoutController::class, 'handleWebhookNotification'])->name('webhooks.mollie-frst');*/
 
 
 Route::get('/mollie/subscribe', [CheckoutController::class, 'newSubscription']);
 Route::post('/molliesubscription', CreateSubscriptionController::class)->name('checkout.plan-mollie');
 
 
-Route::post('/webhooks/mollie', [WebhookController::class, 'handleWebhook'])->name('ng-webhook');
+//Route::post('/webhooks/mollie', [WebhookController::class, 'handleWebhook'])->name('ng-webhook');
 
 // molllie test
 
