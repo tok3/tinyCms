98125160b7e23173419dc0104ca6b53384413062
tachenbach <achenbacher@googlemail.com>
2024-10-29 11:47:09 +0100

    test 1001

--- resources/js/runQualWeb.js
+++ resources/js/runQualWeb.js
@@ -1,14 +1,28 @@
 // Import QualWeb using ES module syntax
 import { QualWeb } from '@qualweb/core';
 
+//const { QualWeb } = require('@qualweb/core');
+
 (async () => {
-  const qualweb = new QualWeb();
+  const qualweb = new QualWeb({
+    puppeteerOptions: {
+      headless: true,
+      executablePath: '/usr/bin/google-chrome',
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',   // Reduces shared memory usage
+        '--disable-gpu',             // Disables GPU acceleration
+        '--window-size=1920,1080',   // Sets a fixed window size
+      ]
+    }
+  });
+
 
   try {
     await qualweb.start();
 
-    // Evaluate a single URL
-    const result = await qualweb.evaluate({ url: 'https://example.com' });
+    const result = await qualweb.evaluate({ url: 'https://acadevo.com' });
 
     console.log('Evaluation result:', JSON.stringify(result, null, 2));
     await qualweb.stop();
@@ -16,3 +30,4 @@ import { QualWeb } from '@qualweb/core';
     console.error('Error evaluating URL:', error.message);
   }
 })();
+
