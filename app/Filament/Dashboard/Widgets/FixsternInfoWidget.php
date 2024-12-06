74c3b621dafbcb5d7458106fb3f547edd2a9c830
tok3 <tobias@eq3w.de>
2024-12-06 15:58:06 +0100

    update button mobilen bottom right

---
+++ app/Filament/Dashboard/Widgets/FixsternInfoWidget.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Dashboard\Widgets;
+
+use Filament\Widgets\Widget;
+use Illuminate\Support\Facades\Auth;
+use Filament\Facades\Filament;
+class FixsternInfoWidget extends Widget
+{
+    protected static ?int $sort = -2;
+
+    protected static bool $isLazy = false;
+
+    /**
+     * @var view-string
+     */
+    protected static string $view = 'filament.dashboard.widgets.fixstern-info-widget';
+}
