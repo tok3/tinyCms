cf5cf8083061e0455f3888c8b7968c6d216f5a45
tachenbach <achenbacher@googlemail.com>
2024-12-20 13:57:18 +0100

    no eval present

--- app/Filament/Resources/Shared/EvaluationResource/Pages/ViewEvaluation.php
+++ app/Filament/Resources/Shared/EvaluationResource/Pages/ViewEvaluation.php
@@ -18,7 +18,27 @@ class ViewEvaluation extends Page
         $latest = Evaluation::where('domainurl_id', request()->route('record'))->latest()->first();
         $domurl = Domainurl::where('id', $latest->domainurl_id)->first();
 
-
+        if($latest == null){
+            return [
+                'evaluation' => '',
+                'url' => $domurl->url,
+                'created_at' => '',
+                'passed' => '',
+                'warning' => '',
+                'failed' => '',
+                'inapplicable' => '',
+            ];
+        } else {
+            return [
+                'evaluation' => json_decode($latest->evaluation, false, 2147483647),
+                'url' => $domurl->url,
+                'created_at' => $latest->created_at,
+                'passed' => $latest->passed,
+                'warning' => $latest->warning,
+                'failed' => $latest->failed,
+                'inapplicable' => $latest->inapplicable,
+            ];
+        }
         /*
         $tmp = json_decode($latest->evaluation, null, 2147483647);
         foreach($tmp->{$domurl->url}->modules as $modulekey => $module) {
@@ -28,15 +48,7 @@ class ViewEvaluation extends Page
         }
         die();
         */
-        return [
-            'evaluation' => json_decode($latest->evaluation, false, 2147483647),
-            'url' => $domurl->url,
-            'created_at' => $latest->created_at,
-            'passed' => $latest->passed,
-            'warning' => $latest->warning,
-            'failed' => $latest->failed,
-            'inapplicable' => $latest->inapplicable,
-        ];
+
     }
 
 
