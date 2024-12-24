a8e0bd38271cfc07f38e26ed4b9de0f50e881d72
tachenbach <achenbacher@googlemail.com>
2024-12-24 05:38:12 +0100

    check created for json null

--- app/Observers/EvaluationObserver.php
+++ app/Observers/EvaluationObserver.php
@@ -7,37 +7,39 @@ use \App\Models\Domainurl;
 use Illuminate\Support\Facades\Log;
 class EvaluationObserver
 {
-    /*
+
     public function created(Evaluation $evaluation)
     {
         $url = Domainurl::where('id', $evaluation->domainurl_id)->first();
         $json = json_decode($evaluation->evaluation);
-        Evaluation::where('id' , $evaluation->id)->update(
-            [
-                'passed' => $json->{$url->url}->metadata->passed,
-                'warning' => $json->{$url->url}->metadata->warning,
-                'failed' => $json->{$url->url}->metadata->failed,
-                'inapplicable' => $json->{$url->url}->metadata->inapplicable,
-                'metadata' => json_encode($json->{$url->url}->metadata),
-            ]
-        );
+        if($json != null){
+            Log::info('no json');
+            Evaluation::where('id' , $evaluation->id)->update(
+                [
+                    'passed' => $json->{$url->url}->metadata->passed,
+                    'warning' => $json->{$url->url}->metadata->warning,
+                    'failed' => $json->{$url->url}->metadata->failed,
+                    'inapplicable' => $json->{$url->url}->metadata->inapplicable,
+                    'metadata' => json_encode($json->{$url->url}->metadata),
+                ]
+            );
 
 
-        // find oldest an latest value and update domainurl
-        $oldest = Evaluation::where('domainurl_id', $evaluation->domainurl_id)->oldest()->first();
-        Domainurl::where('id', $evaluation->domainurl_id)->update(
-            [
-                'passed_oldest' => $oldest->passed,
-                'warning_oldest' => $oldest->warning,
-                'failed_oldest' => $oldest->failed,
-                'inapplicable_oldest' => $oldest->inapplicable,
-                'passed_latest' => $json->{$url->url}->metadata->passed,
-                'warning_latest' => $json->{$url->url}->metadata->warning,
-                'failed_latest' => $json->{$url->url}->metadata->failed,
-                'inapplicable_latest' => $json->{$url->url}->metadata->inapplicable,
-            ]
-        );
-
+            // find oldest an latest value and update domainurl
+            $oldest = Evaluation::where('domainurl_id', $evaluation->domainurl_id)->oldest()->first();
+            Domainurl::where('id', $evaluation->domainurl_id)->update(
+                [
+                    'passed_oldest' => $oldest->passed,
+                    'warning_oldest' => $oldest->warning,
+                    'failed_oldest' => $oldest->failed,
+                    'inapplicable_oldest' => $oldest->inapplicable,
+                    'passed_latest' => $json->{$url->url}->metadata->passed,
+                    'warning_latest' => $json->{$url->url}->metadata->warning,
+                    'failed_latest' => $json->{$url->url}->metadata->failed,
+                    'inapplicable_latest' => $json->{$url->url}->metadata->inapplicable,
+                ]
+            );
+        }
 
     }
     */
