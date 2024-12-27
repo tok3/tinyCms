5a64886bf652ad6ff829f180e09a5e1b8acd8f7d
tok3 <tobias@eq3w.de>
2024-12-27 16:18:06 +0100

    auto add referrer url to pa11y url

--- app/Http/Middleware/VerifyCustomerAccess.php
+++ app/Http/Middleware/VerifyCustomerAccess.php
@@ -6,6 +6,9 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Models\Referrer;
+use App\Models\Pa11yUrl;
+use Illuminate\Support\Facades\Artisan;
+
 class VerifyCustomerAccess
 {
     /**
@@ -33,16 +36,48 @@ class VerifyCustomerAccess
 
         if ($httpReferrer) {
             // Benutze updateOrCreate, um count hochzuzählen
-            Referrer::updateOrInsert(
+            $referrer =  Referrer::updateOrInsert(
                 ['referrer' => $httpReferrer, 'ulid' => $customerUuid], // Suchkriterien
                 [
                     'count' => \DB::raw('count + 1'), // Inkrementiere count
                     'updated_at' => now(), // Aktualisiere updated_at
                 ]
             );
+
+
+            // Wenn ein neuer Referrer erstellt wurde, die URL speichern und den Scan starten
+            if ($referrer && $referrer->wasRecentlyCreated) {
+                $this->createPa11yUrlAndScan($customerUuid, $httpReferrer);
+            }
+
         }
 
         return $next($request);
+    }
+
+    /**
+     * Speichert die URL und startet den Scan für die neue URL.
+     *
+     * @param string $customerUuid
+     * @param string $httpReferrer
+     * @return void
+     */
+    protected function createPa11yUrlAndScan(string $customerUuid, string $httpReferrer)
+    {
+        // Speichern der URL in der Pa11yUrl-Tabelle
+        $pa11yUrl = Pa11yUrl::create([
+            'url' => $httpReferrer,
+            'company_id' => $customerUuid, // Die ID der Company, die dem Referrer zugeordnet ist
+        ]);
+
+        // Logge die URL-ID und starte den Scan für diese URL
+        \Log::info('Starting scan for newly added Pa11yUrl', ['url_id' => $pa11yUrl->id]);
+
+        // Starte den Artisan-Befehl für diese URL
+        Artisan::call('scan:accessibility', [
+            'urls' => [$pa11yUrl->id],  // Nur die neu erstellte URL-ID übergeben
+            '--levels' => 'A,AA,AAA'   // Alle Levels scannen
+        ]);
     }
 
 }
