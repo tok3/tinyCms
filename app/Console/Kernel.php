d9f84c8829d2810332c2e9f4d2127c646d6632e6
tok3 <tobias@eq3w.de>
2024-12-29 17:05:30 +0100

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('scan:accessibility')->daily();
+        $schedule->command('scan:accessibility')->dailyAt('02:00');
 
     }
 
