9efa264dec420261ab7ef02269963799fbcfac64
tok3 <tobias@eq3w.de>
2024-12-30 16:14:25 +0100

    grit mit delete und bulkactions

--- resources/views/accessibility-results.blade.php
+++ resources/views/accessibility-results.blade.php
@@ -68,7 +68,7 @@
                 Errors ({{ $errors }})
             </button>
         </div>
-
+was geht hier ?
         {{-- Issues Cards --}}
         <div class="grid grid-cols-1 md:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
