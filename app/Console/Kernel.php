5e0fef7c473572b151bf361a93da72e1b4b378f3
tok3 <tobias@eq3w.de>
2024-12-30 12:21:07 +0100

    grit mit delete und bulkactions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('scan:accessibility');
+        $schedule->command('scan:accessibility')->dailyAt('23:00');
 
     }
 
