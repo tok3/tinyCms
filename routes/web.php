32e56efe486bef9a8ac062ef26fe14be431a22ae
tok3 <tobias@eq3w.de>
2025-01-29 17:59:25 +0100

    index.html weg

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ use App\Http\Controllers\Pa11yUrlController;
 |
 */
 
+Route::get('/{any}.html', function () {
+    return redirect('/', 301);
+})->where('any', '.*');
 
 // redirect all logins from filament dashbord logins to centralized login
 // Umleitung für /admin/login
