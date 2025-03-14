9417dc60ccde817ecdfe09498a8d6f14a7433e8b
tachenbach <achenbacher@googlemail.com>
2025-03-14 12:53:34 +0100

    storetts

--- app/Http/Controllers/ReferrerController.php
+++ app/Http/Controllers/ReferrerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Referrer;
+use App\Models\Tts;
 use App\Models\DownloadReferrer;
 use Illuminate\Support\Facades\Log;
 
@@ -27,6 +28,24 @@ class ReferrerController extends Controller
         $referrer->referrer = $input['referrer'];
         //$referrer->download = 1;
         $referrer->save();
+        return response()->json([
+            "status" => true,
+            "data" => "OK"
+        ]);
+    }
+
+    public function storeTts(Request $request){
+        $input = $request->json()->all();
+        $record = Tts::firstOrNew(
+            ['ulid' => $input['tts']],
+        );
+
+        if (!$record->exists) {
+            $record->count = 0;
+            $record->save();
+        }
+
+        $record->increment('count');
         return response()->json([
             "status" => true,
             "data" => "OK"
--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'public/assets/css/svgs/*',  'public/assets/fonts/opendyslexic/*', 'storeDownloadReferrer', 'eztext'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'public/assets/css/svgs/*',  'public/assets/fonts/opendyslexic/*', 'storeDownloadReferrer', 'eztext', 'storeTts'],
 
     'allowed_methods' => ['*'],
 
