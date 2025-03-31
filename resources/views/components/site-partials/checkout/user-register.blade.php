b8a51b0bd99b6a6be85dba6ad6c9f8a00adfee1c
tok3 <tobias@eq3w.de>
2025-03-31 15:55:48 +0200

    sepa branch zum testen

--- resources/views/components/site-partials/checkout/user-register.blade.php
+++ resources/views/components/site-partials/checkout/user-register.blade.php
@@ -91,7 +91,7 @@
                     <legend>Abonnement Zahlungsweise</legend>
 
                     <div class="form-group">
-                        <input type="hidden" name="payment_method" value="creditcard" id="payment_creditcard" checked>
+                        <input type="radio" name="payment_method" value="creditcard" id="payment_creditcard" checked>
                         <label for="payment_creditcard">Kreditkarte</label>
                            &nbsp;&nbsp;&nbsp; <input type="radio" name="payment_method" value="sepa" id="payment_sepa">
                         <label for="payment_sepa">SEPA-Lastschrift</label>
