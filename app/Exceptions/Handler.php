a38040edd35a7918e9125b5b795f3dc05a86eb59
tachenbach <achenbacher@googlemail.com>
2025-04-03 09:52:52 +0200

    sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Sentry\Laravel\Integration;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,7 +38,9 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if (app()->bound('sentry')) {
+                Integration::captureUnhandledException($e);
+            }
         });
     }
 }
