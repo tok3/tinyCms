4cff438f5db9184f00965807a24321623d6a9714
tachenbach <achenbacher@googlemail.com>
2025-05-05 09:12:55 +0200

    vergessen

---
+++ app/Filament/Resources/Shared/ContractResource/Pages/ListContracts.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Shared\ContractResource\Pages;
+
+use App\Filament\Resources\Shared\ContractResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListContracts extends ListRecords
+{
+    protected static string $resource = ContractResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
