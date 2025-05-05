83192e6ff7955872401d096309dae0308aa0bb4b
tachenbach <achenbacher@googlemail.com>
2025-05-05 09:13:32 +0200

    contracts

---
+++ app/Filament/Resources/Shared/ContractResource/Pages/CreateContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\Shared\ContractResource\Pages;
+
+use App\Filament\Resources\Shared\ContractResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateContract extends CreateRecord
+{
+    protected static string $resource = ContractResource::class;
+}
---
+++ app/Filament/Resources/Shared/ContractResource/Pages/ViewContract.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\Shared\ContractResource\Pages;
+
+use App\Filament\Resources\Shared\ContractResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewContract extends ViewRecord
+{
+    protected static string $resource = ContractResource::class;
+}
