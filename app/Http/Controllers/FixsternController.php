7f0606e3f584df8760f67f6f06fbfe19d267bf34
tachenbach <achenbacher@googlemail.com>
2025-05-28 07:24:30 +0200

    danish II

--- app/Http/Controllers/FixsternController.php
+++ app/Http/Controllers/FixsternController.php
@@ -47,6 +47,9 @@ class FixsternController extends Controller
                 'en' => "Please translate the following text comment-free into simple language: ".$text,
                 'fr' => "Veuillez traduire le texte suivant sans commentaire en langue simple: ".$text,
                 'it' => "Permetti di tradurre questo testo senza commenti in una lingua semplice: ".$text,
+                'da' => "Oversæt følgende tekst til let dansk. Følg reglerne for let sprog. Brug sprogniveau A1 eller A2. Sæt et linjeskift mellem hver sætning. Undgå fremmedord eller forklar dem. Oversæt ord på fremmedsprog. En 10-årig skal forstå teksten. Skriv forkortelser ud først. Brug derefter forkortelsen i parentes. Lav korte hovedsætninger med cirka 8 ord. Brug kun linjeskift, ingen andre (HTML) koder: ".$text,
+
+"
             );
             //\Log::info("text: ".$text);
 
