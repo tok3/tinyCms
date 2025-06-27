21eb641467acf3a5b80954c072afdbebc1a3b37e
tok3 <tobias@eq3w.de>
2025-06-27 14:35:28 +0200

    create action aus contracts

--- app/Filament/Resources/Admin/ContractResource/Pages/ListContracts.php
+++ app/Filament/Resources/Admin/ContractResource/Pages/ListContracts.php
@@ -13,7 +13,7 @@ class ListContracts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+      //      Actions\CreateAction::make(),
         ];
     }
 }
