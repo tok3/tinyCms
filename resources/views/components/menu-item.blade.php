32a15fd7d27ffe859e8cea5511389a5cba7a1360
tok3 <tobias@eq3w.de>
2025-11-05 16:48:35 +0100

    charset zu fixstern anleitungen hinzugefügt

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,4 +1,4 @@
-<li class="{{ $hasChildren ? 'nav-item dropdown' : 'nav-item' }}" >
+<li class="{{ $hasChildren ? 'nav-item dropdown' : 'nav-item' }} no-hyphen" >
     <a href="{{ $url }}" class="{{ $hasChildren ? 'dropdown-toggle' : 'nav-item' }} nav-link   @if(\Request::segment(1) == $slug || \Request::segment(1) == '' && $slug == '/' || $lastSegment == last(explode('/', $url))  )active @endif {{ $hasChildren ? 'dropdown-toggle' : '' }}" {{ $hasChildren ? 'data-bs-toggle=dropdown' : '' }} target="{{$target}}">{{ $name}}</a>
     @if($hasChildren)
         <div class="dropdown-menu">{!!  $slot !!}</div>
