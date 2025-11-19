8da086b2d36dd3f95fd784082eee703789998e38
tachenbach <achenbacher@googlemail.com>
2025-11-19 13:10:16 +0100

    for older mysql versions again

--- database/migrations/2025_10_22_163355_add_text_fields_to_acc_comp_declarations.php
+++ database/migrations/2025_10_22_163355_add_text_fields_to_acc_comp_declarations.php
@@ -13,28 +13,28 @@ return new class extends Migration
     {
         Schema::table('acc_comp_declarations', function (Blueprint $table) {
             if (!Schema::hasColumn('acc_comp_declarations', 'consistency')) {
-                $table->string('consistency', 20000)->after('company_offer_ez')->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
+                $table->string('consistency', 16000)->after('company_offer_ez')->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
             }
             if (!Schema::hasColumn('acc_comp_declarations', 'consistency_ez')) {
-                $table->string('consistency_ez', 20000)->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
+                $table->string('consistency_ez', 16000)->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
             }
             if (!Schema::hasColumn('acc_comp_declarations', 'bfsg_full')) {
-                $table->string('bfsg_full', 20000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
+                $table->string('bfsg_full', 16000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
             }
             if (!Schema::hasColumn('acc_comp_declarations', 'bfsg_full_ez')) {
-                $table->string('bfsg_full_ez', 20000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
+                $table->string('bfsg_full_ez', 16000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
             }
             if (!Schema::hasColumn('acc_comp_declarations', 'bfsg_partial')) {
-                $table->string('bfsg_partial', 20000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
+                $table->string('bfsg_partial', 16000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
             }
             if (!Schema::hasColumn('acc_comp_declarations', 'bfsg_partial_ez')) {
-                $table->string('bfsg_partial_ez', 20000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
+                $table->string('bfsg_partial_ez', 16000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
             }
             if (!Schema::hasColumn('acc_comp_declarations', 'non_conform_content')) {
-                $table->string('non_conform_content', 20000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
+                $table->string('non_conform_content', 16000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
             }
             if (!Schema::hasColumn('acc_comp_declarations', 'non_conform_content_ez')) {
-                $table->string('non_conform_content_ez', 20000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
+                $table->string('non_conform_content_ez', 16000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
             }
         });
     }
--- database/migrations/2025_10_27_121404_add_fields_to_acc_declarations_table.php
+++ database/migrations/2025_10_27_121404_add_fields_to_acc_declarations_table.php
@@ -13,31 +13,31 @@ return new class extends Migration
     {
         Schema::table('acc_declarations', function (Blueprint $table) {
             if (!Schema::hasColumn('acc_declarations', 'consistency')) {
-                $table->string('consistency', 20000)->after('category')->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
+                $table->string('consistency', 16000)->after('category')->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
             }
             if (!Schema::hasColumn('acc_declarations', 'consistency_ez')) {
-                $table->string('consistency_ez', 20000)->after('consistency')->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
+                $table->string('consistency_ez', 16000)->after('consistency')->nullable()->default('Unsere Produkte und Dienstleistungen sind für Menschen mit Behinderungen in der allgemein üblichen Weise, ohne besondere Erschwernis und grundsätzlich ohne fremde Hilfe auffindbar, zugänglich und nutzbar.');
             }
             if (!Schema::hasColumn('acc_declarations', 'bfsg_full')) {
-                $table->string('bfsg_full', 20000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
+                $table->string('bfsg_full', 16000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
             }
             if (!Schema::hasColumn('acc_declarations', 'bfsg_full_ez')) {
-                $table->string('bfsg_full_ez', 20000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
+                $table->string('bfsg_full_ez', 16000)->nullable()->default('Unsere Webseite ist mit dem BFSG und der BFSGV vereinbar; alle Anforderungen werden erfüllt.');
             }
             if (!Schema::hasColumn('acc_declarations', 'bfsg_partial')) {
-                $table->string('bfsg_partial', 20000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
+                $table->string('bfsg_partial', 16000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
             }
             if (!Schema::hasColumn('acc_declarations', 'bfsg_partial_ez')) {
-                $table->string('bfsg_partial_ez', 20000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
+                $table->string('bfsg_partial_ez', 16000)->nullable()->default('Unsere Webseite ist in großen Teilen mit dem BFSG und der BFSGV vereinbar. Jedoch bestehen noch einige Barrieren auf unseren Seiten, an denen wir aktiv arbeiten und diese in Zukunft beseitigen wollen. Folgende Ausnahmen und Unvereinbarkeiten bestehen:');
             }
             if (!Schema::hasColumn('acc_declarations', 'non_conform_content')) {
-                $table->string('non_conform_content', 20000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
+                $table->string('non_conform_content', 16000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
             }
             if (!Schema::hasColumn('acc_declarations', 'non_conform_content_ez')) {
-                $table->string('non_conform_content_ez', 20000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
+                $table->string('non_conform_content_ez', 16000)->nullable()->default('Die folgenden Inhalte sind nicht barrierefrei, da Sie eine unverhältnismäßige Belastung gemäß § 12a Absatz 6 BGG darstellen:');
             }
             if (!Schema::hasColumn('acc_declarations', 'scope')) {
-                $table->string('scope', 20000)->after('federal_state')->nullable()->default();
+                $table->string('scope', 16000)->after('federal_state')->nullable()->default();
             }
             if (!Schema::hasColumn('acc_declarations', 'published')) {
                 $table->tinyInteger('published')->after('json_eztext')->default(0);
