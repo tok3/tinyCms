ca33886f95edbe6a0ae9566f4bbb3c7d5a1985de
tok3 <tobias@eq3w.de>
2026-03-30 19:03:30 +0200

    email update

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-xl-4 col-lg-5 col-md-6 col-sm-8 z-2">
 
                     <h2 class="mb-1 display-6">
-                       Passwort vergessen?
+                        Passwort vergessen?
                     </h2>
                     <p class="mb-4 text-body-secondary">
                         Kein Problem. Teilen Sie uns einfach Ihre E-Mail-Adresse mit, und wir senden Ihnen einen Link zum Zurücksetzen des Passworts.
@@ -16,32 +16,41 @@
                     <div class="position-relative">
                         <div>
                             <!-- Session Status -->
-                            <x-auth-session-status class="mb-4" :status="session('status')" />
+                            <x-auth-session-status class="mb-4" :status="session('status')"/>
 
-                            <form method="POST" novalidate  action="{{ route('password.email') }}">
-                                @csrf
-                                <div class="input-icon-group mb-3">
-                                            <span class="input-icon">
-                                          <i class="bx bx-envelope"></i>
-                                        </span>
+                            @if (!session('status'))
+                                <form method="POST" novalidate action="{{ route('password.email') }}">
+                                    @csrf
 
+                                    <div class="input-icon-group mb-3">
+                    <span class="input-icon">
+                        <i class="bx bx-envelope"></i>
+                    </span>
 
-                                    <!-- Email Address -->
-                                    <div>
-                                        <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" :placeholder="__('Email')"/>
-
+                                        <div>
+                                            <x-text-input
+                                                id="email"
+                                                class="form-control"
+                                                type="email"
+                                                name="email"
+                                                :value="old('email')"
+                                                required
+                                                autofocus
+                                                autocomplete="username"
+                                                :placeholder="__('Email')"
+                                            />
+                                        </div>
                                     </div>
-                                </div>
 
-                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2"/>
 
-                                <div class="d-grid">
-                                    <x-primary-button class="btn btn-primary">
-                                      Passwort zurücksetzen Link <senden></senden>
-                                    </x-primary-button>
-
-                                </div>
-                            </form>
+                                    <div class="d-grid">
+                                        <x-primary-button class="btn btn-primary">
+                                            Passwort zurücksetzen Link senden
+                                        </x-primary-button>
+                                    </div>
+                                </form>
+                            @endif
 
 
                         </div>
@@ -50,7 +59,6 @@
             </div>
         </div>
     </section>
-
 
 
 </x-page-layout>
--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,10 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'alert alert-success']) }}>
+        <h5>Vielen Dank,</h5>
+        <p>
         {{ $status }}
+        </p>
     </div>
 @endif
