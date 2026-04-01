a9a2a0aa6190e29ca3684eb962fe85d84dbebb32
tok3 <tobias@eq3w.de>
2026-04-01 10:53:30 +0200

    DemoUrlDiscoveryService better detection

--- resources/views/components/partials/preloader.blade.php
+++ resources/views/components/partials/preloader.blade.php
@@ -1,5 +1,5 @@
  <!--Preloader Spinner-->
- <div class="spinner-loader bg-white text-gray-400">
+ <div class="spinner-loader _bg-white text-gray-200" style=" background: rgba(255, 255, 255, 0.5) !important;">
     <div class="spinner-border" role="status">
     </div>
 </div>
