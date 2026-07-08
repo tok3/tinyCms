50a89e932f1a17e89a27163c813c4418d41e21fe
tachenbach <achenbacher@googlemail.com>
2026-07-08 10:17:50 +0200

    Fix service route registration

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,7 +33,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::group(base_path('routes/service.php'));
+            Route::group([], base_path('routes/service.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
